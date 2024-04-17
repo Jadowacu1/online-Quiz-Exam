f5a27dfe58b2442a71629875c511aec1277150dd
Jadowacu1 <152473876+Jadowacu1@users.noreply.github.com>
2024-04-17 15:28:51 +0200

    Java codes

---
+++ src/Quiz.java
@@ -0,0 +1,20 @@
+
+package quizeapplication;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Quiz {
+    private List<Question> questions;
+
+    public Quiz() {
+        questions = new ArrayList<>();
+    }
+
+    public void addQuestion(Question question) {
+        questions.add(question);
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+}
